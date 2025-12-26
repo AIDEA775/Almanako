3aa3dedcc16f39d33660d0dcaa1a3afe783d62d6
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2025-12-26 04:10:21 -0300

    file picker more pretty

--- src/Holidays.svelte
+++ src/Holidays.svelte
@@ -11,14 +11,15 @@
 </script>
 
 <div
-    class="flex flex-col gap-y-1 my-8
-    items-center justify-center"
+    class="flex flex-col gap-y-1 my-8 justify-center"
 >
     <p class="text-stone-500">Add events to calendar with .ics files</p>
 
-    <div class="flex flex-col gap-2">
+    <div class="flex flex-wrap justify-center gap-2">
         {#each $icsList as icsFile}
-            <div class="flex flex-row items-center group">
+            <div class="flex flex-col items-center">
+                <label class="font-medium text-stone-700 cursor-pointer py-1"
+                    for={icsFile.label}>{icsFile.label}</label>
                 <input
                     type="file"
                     accept="text/calendar"
@@ -28,14 +29,13 @@
                         icsFile.data = await parse(ev.target.files);
                     }}
                     title="Select a .ICS file with birthdays."
-                    class="text-sm text-stone-500
-                        file:mr-2 file:py-2 file:px-4
-                        file:rounded-full file:border file:border-solid file:border-stone-200
+                    class="text-sm text-stone-600 cursor-pointer
+                        file:mr-2 file:py-3 file:px-4
                         file:text-sm
                         file:bg-stone-100 file:text-stone-700
-                        group-hover:file:bg-stone-200"
+                        rounded-full border border-solid border-stone-200
+                        bg-stone-50 hover:file:bg-stone-200"
                 />
-                <div class="font-medium text-stone-700">{icsFile.label}</div>
             </div>
         {/each}
     </div>
